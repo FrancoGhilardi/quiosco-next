cccdc3dc87cd19670af3a1bc0965900f4a0107e2
franco <franco,ghilardi1@gmail.com>
2024-11-04 21:15:38 -0300

    Se termina con pedido de productos desde el cliente

---
+++ components/ui/ToastNotification.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+import "react-toastify/dist/ReactToastify.min.css";
+import { ToastContainer } from "react-toastify";
+
+const ToastNotification: React.FC = () => {
+  return <ToastContainer />;
+};
+
+export default ToastNotification;
